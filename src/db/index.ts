3dbe383ca01e7c458924a3b68d3161ae87e4242a
jeeraffo <thesafatem@gmail.com>
2024-05-27 20:18:03 +0500

    Fix get mongo string

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose';
 
 const getConnectionUri = (): string => {
-    return process.env.MONGO_URI || 'mongodb://telecomUser:password@localhost:27017/telecom'
+    return 'mongodb://' +
+        process.env.MONGO_USER +
+        ':' + process.env.MONGO_PASSWORD +
+        '@' + process.env.MONGO_HOST +
+        ':' + process.env.MONGO_PORT +
+        '/' + process.env.MONGO_DB
 }
 const createConnection = () => {
     return async (): Promise<void> => {
